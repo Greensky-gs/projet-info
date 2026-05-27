c087ce9592a83a30c333b042ddabe5609af58d73
Greensky-gs <david.hesliere@gmail.com>
2026-05-27 17:06:10 +0200

    Work on improved bot

---
+++ src/computer/engine/select_move.py
@@ -0,0 +1,108 @@
+from _headers.constants import *
+from moves.detection import *
+from moves.capture import *
+from random import randint
+from moves.move import deplacement_mouvement
+from suddendeath.detection import appliquer_mort_subite
+from structs.heap import *
+import logging
+# Fonction de sélection de coup pour l'ordinateur
+
+def score_position(grille, joueur):
+    """
+    Donne un score à une position donnée
+    Entrée: grille, joueur
+        grille : La position
+        joueur : Le joueur qui représente l'ordinateur
+
+    Sortie: int - entier représentant le score de la position. Une valeur négative est une position désavantageuse pour l'ordinateur
+    """
+
+    # Analyse simplement matérielle
+    pions_ordinateur = 0
+    pions_adversaire = 0
+    couleur_adversaire = joueur_adverse(joueur)
+
+    for x in range(N):
+        for y in range(N):
+            # Pour des questions d'optimisation, on évite les caluls inutiles (ex: vérification de coordonnées)
+            if grille[x][y] == couleur_adversaire:
+                pions_adversaire += 1
+            elif grille[x][y] == joueur:
+                pions_ordinateur += 1
+
+    return pions_ordinateur - pions_adversaire
+
+def ligne_str(ligne, coup):
+    """Représente un ligne de coups sous forme de texte"""
+    return ligne + "{}{}{}{}".format(coup[0], coup[1], coup[2], coup[3])
+
+def analyse_libre(grille, joueur):
+    """
+    Analyse la grille librement (sans devoir jouer un pion en particulier)
+    Entrée: grille, joueur
+        grille : la grille
+        joueur : la couleur qui représente l'ordinateur
+
+    Sortie: tuple[bool, int, int, int, int] - Si c'est une capture, et le coup choisit par l'ordinateur
+    """
+    
+    scores = abr_creer(8192)
+
+    # On trouve tous les coups autorisés, puis on les "joue", et on assigne un score à chacune des grilles, en gardant seulement la meilleure
+    captures = detection_captures_joueur(grille, joueur)
+    if len(captures) > 0:
+        for capture in captures:
+            captures_pion = detection_captures_pions(grille, capture)
+            for capture_pion in captures_pion:
+                clone = [row[:] for row in grille]
+
+                deplacement_capture(clone, capture[0], capture[1], capture_pion[0], capture_pion[1], joueur)
+                appliquer_mort_subite(clone, joueur)
+
+                score = score_position(clone, joueur)
+                abr_inserer(scores, [score, ligne_str("", (capture[0], capture[1], capture_pion[0], capture_pion[1]))])
+    else:
+        moves = detection_deplacements_joueur(grille, joueur)
+        for move in moves:
+            moves_pion = detection_deplacements_pions(grille, move)
+            for move_pion in moves_pion:
+                clone = [row[:] for row in grille]
+
+                deplacement_mouvement(clone, move[0], move[1], move_pion[0], move_pion[1], joueur)
+                appliquer_mort_subite(clone, joueur)
+
+                score = score_position(clone, joueur)
+                abr_inserer(scores, [score, ligne_str("", (move[0], move[1], move_pion[0], move_pion[1]))])
+
+    meilleur_coup = abr_sommet(scores)
+    if meilleur_coup is None:
+        return None
+
+    return (
+            len(captures) > 0,
+            int(meilleur_coup[1][0]),
+            int(meilleur_coup[1][1]),
+            int(meilleur_coup[1][2]),
+            int(meilleur_coup[1][3])
+    )
+
+def choisir_coup_ordinateur_ameliore(grille, joueur, pion_impose = None) -> tuple[int, tuple[int, int], tuple[int, int]] | None:
+    """
+    Fait choisir un coup à l'ordinateur, en respectant les règles
+    Entrée : grille, joueur, pion_impose
+        grille      : la grille de jeu
+        joueur      : la couleur que l'ordinateur joue
+        pion_impose : Le pion que l'ordinateur est obligé de jouer dans le cas où il doit effectuer des prises successives. Vaut None quand l'ordinateur fait son premier coup du tour
+
+    Sortie : tuple[int, tuple[int, int], tuple[int, int]] | None - Le type de déplacement (0 = déplacement, 1 = capture) Les coordonnées du pion d'origine et celles de la case d'arrivée/celle du pion à capturer, ou None si l'ordinateur ne trouve aucun coup
+    """
+    if pion_impose is None:
+        coup = analyse_libre(grille, joueur)
+        if coup is None:
+            return None
+        return (1 if coup[0] else 0, (coup[1], coup[2]), (coup[3], coup[4]))
+    else:
+        captures = detection_captures_pions(grille, pion_impose)
+        if len(captures) == 0:
+            return None
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from random import randint
 from time import sleep
 from _headers._header import *
 from _headers.constants import *
+from computer.engine.play import jeu_ordinateur_ameliore
 from computer.naive.play import jeu_ordinateur
 from structs.grille.helpers import *
 from structs.user.interface import *
@@ -16,8 +17,8 @@ def jcj(grille: list[list[int]]):
 
     Prend en paramètre la grille sélectionnée
     """
-    nomJ1 = input("\x1b[4mJoueur 1 entrez votre pseudo :\x1b[0m ")
-    nomJ2 = input("\x1b[4mJoueur 2 entrez votre pseudo :\x1b[0m ")
+    nomJ1 = ask_pseudo("\x1b[4mJoueur 1 entrez votre pseudo :\x1b[0m ")
+    nomJ2 = ask_pseudo("\x1b[4mJoueur 2 entrez votre pseudo :\x1b[0m ")
 
     couleurJ1 = randint(1, 2)
 
@@ -74,13 +75,13 @@ def jcj(grille: list[list[int]]):
         nom_gagnant = nomJ2 if tour == couleurJ1 else nomJ1
         print(f"\x1b[1m{nom_gagnant}\x1b[0m a gagné !")
 
-def jco(grille):
+def jco(grille, est_ordinateur_ameliore):
     """
     Fonction principale pour le joueur contre ordinateur
 
     Prend en paramètre la grille sélectionnée
     """
-    nomJ1 = input("\x1b[4mJoueur, entrez votre pseudo :\x1b[0m ")
+    nomJ1 = ask_pseudo("\x1b[4mJoueur, entrez votre pseudo :\x1b[0m ")
     nomJ2 = "FryDames1"
 
     couleurJ1 = randint(1, 2)
@@ -104,11 +105,13 @@ def jco(grille):
 
     afficher_grille(grille, tour, noms)
 
+    fonction_appel = jeu_ordinateur if not est_ordinateur_ameliore else jeu_ordinateur_ameliore
+
     while not res[0]:
         if tour == couleurJ1:
             mort_subite = tour_de_jeu(grille, tour, noms)
         else:
-            mort_subite = jeu_ordinateur(grille, tour, nomJ2, tour, noms)
+            mort_subite = fonction_appel(grille, tour, nomJ2, tour, noms)
 
         tour = inverser_tour(tour)
 
@@ -163,9 +166,10 @@ if __name__ == "__main__": # Condition permettant d'être excuté seulement en l
         grille_fin[4][3] = 2
 
         grille_milieu = [ [ 0 for _ in range(N) ] for _ in range(N) ]
-        for a, b in [ (0, 5), (0, 7), (1, 6), (1, 4), (2, 1), (2, 7), (3, 6) ]:
+        for a, b in [ (0, 5), (0, 7), (1, 4), (2, 7), (3, 2), (3, 4), (3, 6) ]:
             set_case(grille_milieu, a, b, 2)
-        for a, b in [ (4, 7), (5, 0), (5, 2), (5, 4), (6, 1), (6, 3), (6, 7), (7, 0) ]:
+        # TODO remove (2, 5)
+        for a, b in [ (2, 5), (4, 7), (5, 0), (5, 2), (5, 4), (6, 1), (6, 3), (6, 7), (7, 0) ]:
             set_case(grille_milieu, a, b, 1)
     
         grille = [grille_depart, grille_milieu, grille_fin][grille_selectionne]
@@ -173,4 +177,9 @@ if __name__ == "__main__": # Condition permettant d'être excuté seulement en l
         if option_selectionnee == 1:
             jcj(grille);
         else:
-            jco(grille)
+            ordinateur_selectionne = menu("Ordinateur", [
+                ("Naïf", "Affrontez l'ordinateur naïf", 0),
+                ("Amélioré", "Affrontez l'ordinateur doté d'un système de raisonnement", 1)
+            ])
+
+            jco(grille, ordinateur_selectionne == 1)
